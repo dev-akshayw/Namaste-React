4f50c622d946188bab339d7108321ebae672cde3
Akshay Waychal <webtechakshay5665@gmail.com>
2024-04-09 00:43:14 +0530

    React Basics

---
+++ App.js
@@ -0,0 +1,19 @@
+    // const head = document.createElement("h1"); 
+    // head.innerHTML = "Namaste Everyone"; 
+    // const root = document.getElementById("root");
+
+    // root.appendChild(head);l
+
+    const head =  React.createElement("h1", {
+        id: "hello",
+    } , "Namaste React"); 
+
+    const head2 = React.createElement("h2", {
+        class: "h2",
+    }, "heading h2"); 
+
+    const container = React.createElement("container", {}, [head, head2]);
+
+    const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+    root.render(container);
