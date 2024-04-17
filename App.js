82137fe017c7b278111c40673ae0f6ba9b8a7174
Akshay <webtechakshay5665@gmail.com>
2024-04-18 00:17:30 +0530

    Ignite-our-App

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-    // const head = document.createElement("h1"); 
-    // head.innerHTML = "Namaste Everyone"; 
-    // const root = document.getElementById("root");
-
-    // root.appendChild(head);l
+import React from "react";
+import ReactDOM from "react-dom/client";
 
     const head =  React.createElement("h1", {
         id: "hello",
     } , "Namaste React"); 
 
     const head2 = React.createElement("h2", {
-        class: "h2",
+        className: "h2",
     }, "heading h2"); 
 
-    const container = React.createElement("container", {}, [head, head2]);
+    const container = React.createElement("div", {}, [head, head2]);
 
     const root = ReactDOM.createRoot(document.getElementById("root")); 
 
