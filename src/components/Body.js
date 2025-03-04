e3c61ad4ad8fd0a38585507237e986aec07cf9ba
Akshay Waychal <webtechakshay5665@gmail.com>
2025-03-04 10:55:23 +0530

    before-npm-router

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,9 +18,11 @@ export const Body = () => {
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7049873&lng=74.24325270000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         const json = await data.json();
+        // console.log(json);
         setlistOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilterRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
+    console.log(listOfRestaurants);
 
     return listOfRestaurants.length === 0 ? (
         <Shimmer />
@@ -44,7 +46,7 @@ export const Body = () => {
 
                 <button className="filter-btn" onClick={() =>{
                     let filterList = listOfRestaurants.filter((res) => res.info.avgRating > 4.4 );
-                    console.log(filterList);
+                    setFilterRestaurant(filterList);
                 }}>
                     Top Rated Customers
                 </button>
